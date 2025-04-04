ee7b4526db2fec7e0ef4a860dc5e0e4261b98d91
Arnav Verma <arnavverma3016@gmail.com>
2025-04-04 14:40:56 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const express = require('express');
-const { resolve } = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const User=require('./Models/User');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 8000;
+app.use(express.json());
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+
+
+app.post('/login', async(req,res) => {
+  try{
+    const {mail, password} = req.body;
+
+    if(!mail || !password){
+      return res.status(400).json({message: 'Email and Password are required'});
+    }
+
+    const user = await User.findOne({mail});
+    if(!user){
+      return res.status(404).json({message: "User not found"});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return res.status(401).json({message: "Invalid credentials"});
+    }
+
+    res.status(200).json({ message: "Login Successful", user: {id: user._id, email: user.mail}});
+  } catch(error){
+    res.status(500).json({message: 'Server error', error: error.message});
+  }
 });
 
+mongoose.connect('mongodb+srv://iashishyadav17:test12@cluster0.8yz77i5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  
+
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const PORT = port || 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
